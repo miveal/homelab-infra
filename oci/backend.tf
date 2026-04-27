b5223a65cf0a03086bf7baead9285697b38c4051
miveal <ziralls@gmail.com>
2026-04-27 11:43:14 +0200

    fix: namespace

--- oci/backend.tf
+++ oci/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "oci" {
     bucket    = "homelab-tfstate"
-    namespace = "https://frpbrwkcbszf.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
+    namespace = "frpbrwkcbszf"
     key       = "oci/terraform.tfstate"
   }
 }
