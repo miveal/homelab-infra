22ef465af1ec9d344558a189e1b88596874fe72d
miveal <ziralls@gmail.com>
2026-04-27 11:34:27 +0200

    fix: kolejny backend fix

--- oci/backend.tf
+++ oci/backend.tf
@@ -1,13 +1,8 @@
 terraform {
-  backend "s3" {
-    # OCI Object Storage kompatybilny z S3 API
-    bucket                      = "homelab-tfstate"
-    key                         = "oci/terraform.tfstate"
-    region                      = "eu-frankfurt-1"
-    endpoint                    = "https://frpbrwkcbszf.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
+  backend "oci" {
+    bucket    = "homelab-tfstate"
+    namespace = "https://frpbrwkcbszf.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
+    key       = "oci/terraform.tfstate"
+    region    = "eu-frankfurt-1"
   }
 }
--- oci/providers.tf
+++ oci/providers.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">=1.12"
+
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 6.0"
+      version = ">= 7.0.0"
     }
   }
 }
